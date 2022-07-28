4333439bbb6e96d5b7fc47173b5ceb6436b2203b
Chrystian Santos <chrystian_santos_silva@hotmail.com>
2022-07-28 18:45:57 -0300

    feat: page home

--- new_ignite/project_2/src/pages/Home/index.tsx
+++ new_ignite/project_2/src/pages/Home/index.tsx
@@ -1,3 +1,46 @@
+import { Play } from 'phosphor-react';
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+} from './styles';
+
 export function Home() {
-  return <h1>Home</h1>;
+  return (
+    <HomeContainer>
+      <form>
+        <FormContainer>
+          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput
+            id="task"
+            type="text"
+            placeholder="Dê um nome para o seu projeto"
+          />
+          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmountInput
+            id="minutesAmount"
+            type="number"
+            placeholder="00"
+          />
+          <span>minutos.</span>
+        </FormContainer>
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+        <StartCountdownButton disabled type="submit">
+          <Play size={24} /> Começãr
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  );
 }
